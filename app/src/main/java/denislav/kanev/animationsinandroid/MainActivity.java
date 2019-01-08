05dc6768b9408b990ac0ae1305f2a53744e9ca0c
Denislav Kanev <denk@itu.dk>
2019-01-08 16:43:47 +0200

    Show UI Button added

--- app/src/main/java/denislav/kanev/animationsinandroid/MainActivity.java
+++ app/src/main/java/denislav/kanev/animationsinandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtHiWorld;
     private TextView txtAndroid;
     private boolean isHelloWorldShowing = true;
+    private Button btnAnimate;
 
 
     @Override
@@ -24,13 +26,20 @@ public class MainActivity extends AppCompatActivity {
         txtHelloWorld = findViewById(R.id.txtHelloWorld);
         txtHiWorld = findViewById(R.id.txtHiWorld);
         txtAndroid = findViewById(R.id.txtAndroid);
+        btnAnimate = findViewById(R.id.btnAnimate);
 
+        txtHelloWorld.setX(2000);
+        txtHiWorld.setX(-2000);
+        txtAndroid.setY(-2000);
 
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 //Log.i("MYTAG", "Hello World!");
                 //txtHelloWorld.animate().alpha(0.5f).setDuration(3000);
+
                 if(isHelloWorldShowing){
                     txtHelloWorld.animate().alpha(0).setDuration(3000);
                     isHelloWorldShowing = false;
@@ -49,8 +58,19 @@ public class MainActivity extends AppCompatActivity {
         txtAndroid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtAndroid.animate().translationXBy(3000).setDuration(2000);
-                txtAndroid.animate().rotation(7200).setDuration(2000);
+                //txtAndroid.animate().translationXBy(3000).setDuration(2000);
+                //txtAndroid.animate().rotation(7200).setDuration(2000);
+                //txtAndroid.animate().rotationBy(90).setDuration(4000);
+                txtAndroid.animate().rotationX(400).setDuration(3000);
+            }
+        });
+
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHelloWorld.animate().translationXBy(-2000).rotation(2000).setDuration(1000);
+                txtHiWorld.animate().translationXBy(2000).rotation(-2000).setDuration(1500);
+                txtAndroid.animate().translationYBy(3000).rotationBy(90).setDuration(2000);
             }
         });
 
