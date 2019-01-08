519a1234c868dfec130632405f12426029da2538
Denislav Kanev <denk@itu.dk>
2019-01-08 15:52:01 +0200

    Text-swap fade animation working

--- app/src/main/java/denislav/kanev/animationsinandroid/MainActivity.java
+++ app/src/main/java/denislav/kanev/animationsinandroid/MainActivity.java
@@ -2,12 +2,47 @@ package denislav.kanev.animationsinandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHelloWorld;
+    private TextView txtHiWorld;
+    private boolean isHelloWorldShowing = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHelloWorld = findViewById(R.id.txtHelloWorld);
+        txtHiWorld = findViewById(R.id.txtHiWorld);
+
+
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i("MYTAG", "Hello World!");
+                //txtHelloWorld.animate().alpha(0.5f).setDuration(3000);
+                if(isHelloWorldShowing){
+                    txtHelloWorld.animate().alpha(0).setDuration(3000);
+                    isHelloWorldShowing = false;
+                    txtHiWorld.animate().alpha(1).setDuration(3000);
+                } else {
+                    txtHiWorld.animate().alpha(0).setDuration(3000);
+                    txtHelloWorld.animate().alpha(1).setDuration(3000);
+                    isHelloWorldShowing = true;
+                }
+
+
+
+            }
+        });
+
     }
 }
